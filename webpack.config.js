11a7406f8c496e13ef228a652753f2d7faa65f4e
Johan Lindell <johan@lindell.me>
2017-02-20 20:54:07 +0100

    Added es2015 preset to babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         exclude: [/node_modules/],
+        query: {
+          presets: ['es2015']
+        }
       }
     ]
   }
